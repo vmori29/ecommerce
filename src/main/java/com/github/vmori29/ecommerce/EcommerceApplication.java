0cb456b2745e7b2e0115bbfcfc9ead69ae0c8868
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-23 19:49:04 -0300

    first commit

---
+++ src/main/java/com/github/vmori29/ecommerce/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.github.vmori29.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/vmori29/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vmori29.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
